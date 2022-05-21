9116076f9006a38c9f2e8f1b599105619da7d04b
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-05-21 10:36:45 +0100

    Created add to do list function

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const addItem = document.getElementById("add-btn");
 const newInput = document.getElementById("input-text");
 const tasks = document.getElementById("tasks");
 
+function addToDo() {
+  let text = newInput.value;
+  tasks.innerHTML += ` 
+                <div class="item">
+                 <input class="item-check" type="checkbox" />
+                 <input type="text" class="item-text" readonly value="${newInput.value}"></input>
+                 <div class="action-btn">
+                     <span class="edit-btn"><i class="bi bi-pencil"></i></span>
+                     <span class="save-btn" style="display:none;"><i class="bi bi-check-circle"></i></span>
+                     <span class="delete-btn"><i class="bi bi-trash3"></i></span>
+                 </div>
+              </div>
+           `;
